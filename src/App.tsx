4081c1ec7fc03acba0d2e01c86c54c1656b5961a
Abdul <abennett56@gmail.com>
2026-09-14 19:19:31 +0100

    Began creating the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className='main-container'>
-      <strong>Abi's Big Shripplies</strong>
+    <div className="main-container">
+      <img className="logo-backdrop" src="./assets/Logo with Name Transparent.png" alt="Logo for Sāwol sussex" />
+      <div className="landing-page-spinner-container">
+        <strong className='text-align-right'>Website coming soon</strong>
+        <img className="loading-star" src="./assets/Star.png" />
+        <strong>Find us on socials</strong>
+      </div>
     </div>
   );
 }
